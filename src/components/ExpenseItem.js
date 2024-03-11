6351ab1e495775543e85bcd087db8eb4bd15ecd0
anothercod <tw19970520@gmail.com>
2024-03-11 18:40:41 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Alert.css 	new file:   src/components/Alert.js 	modified:   src/components/ExpenseForm.js 	modified:   src/components/ExpenseItem.css 	modified:   src/components/ExpenseItem.js 	modified:   src/components/ExpenseList.css 	modified:   src/components/ExpenseList.js 	modified:   src/index.css 	modified:   src/index.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react'
-import { MdEdit } from 'react-icons/md';
-import './ExpenseItem.css'
+import React from 'react'
+import { MdEdit, MdDelete } from 'react-icons/md';
+import './ExpenseItem.css';
 
-export default class ExpenseItem extends Component {
-  render() {
-    return (
-      <li className="item">
-        <div className="info">
-            <span className="expense"></span>
-            <span className="amount"> 원 </span>
-        </div>
-        <div>
-            <button className="edit-btn">
-                <MdEdit/>
-            </button>
-            <button className="clear-btn">
-                <MdEdit/>
-            </button>
-        </div>
+const ExpenseItem = ({ expense, handleDelete, handleEdit }) => {
+  return (
+      <li className='item'>
+          <div className='info'>
+              <span className='expense'>{expense.charge}</span>
+              <span className='amount'>{expense.amount}</span>
+          </div>
+          <div>
+              <button className='edit-btn'
+                  onClick={() => handleEdit(expense.id)}
+              >
+                  <MdEdit />
+              </button>
+              <button className='clear-btn'
+                  onClick={() =>
+                      handleDelete(expense.id)
+                  }
+              >
+                  <MdDelete />
+              </button>
+          </div>
       </li>
-    );
-  };
-};
+  )
+
+}
+
+export default ExpenseItem
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { MdDelete } from 'react-icons/md';
 import './ExpenseList.css'
+import ExpenseItem from './ExpenseItem';
 
-export default class ExpenseList extends Component {
-  render() {
-    return (
+
+const ExpenseList = ({ handleDelete, expenses, handleEdit, clearItems }) => {
+  return (
       <>
-        <ul className="list">
-            {/* Expense Item*/}
-        </ul>
-        <button className="btn">
-            목록 지우기
-            <MdDelete className="btn-icon" />
-        </button>
+          <ul className='list'>
+              {/* Expense Item */}
+              {expenses.map(expense => {
+                  return (
+                      <ExpenseItem
+                          expense={expense}
+                          key={expense.id}
+                          handleDelete={handleDelete}
+                          handleEdit={handleEdit}
+                      />
+                  )
+              })}
+          </ul>
+          {expenses.length > 0 && (
+              <button className='btn' onClick={clearItems}>
+                  목록 지우기
+                  <MdDelete className='btn-icon' />
+              </button>
+          )}
       </>
-    );
-  };
-};
+  )
+}
+
+export default ExpenseList
